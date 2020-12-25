5772a69747131977a8a3d1728b9c2d74ae564f00
张朋 <zhangpeng566@jd.com>
2020-12-25 17:48:58 +0800

    开发交互

--- src/components/activityList/cardSpecial.vue
+++ src/components/activityList/cardSpecial.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="cardSpecial">
-    <el-card :body-style="{ padding: '0px' }">
+    <el-card :body-style="{ padding: '0px' }" shadow="hover">
       <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
       <div style="padding: 14px;">
         <span>好吃的汉堡</span>
         <div class="bottom clearfix">
-          <time class="time">{{ currentDate }}</time>
+          <time class="time">2021-1-1</time>
           <el-button type="text" class="button">操作按钮</el-button>
         </div>
       </div>
--- src/views/activity/activity.vue
+++ src/views/activity/activity.vue
@@ -5,71 +5,113 @@
       <div class="title">
         <span>酷车闪购</span>
         <div class="btn">
-          <el-button @click="left" size="mini" icon="el-icon-arrow-left"></el-button>
-          <el-button @click="right" size="mini" icon="el-icon-arrow-right"></el-button>
+          <el-button ref="leftBtn" disabled @click="left" size="mini" icon="el-icon-arrow-left"></el-button>
+          <el-button ref="rightBtn" @click="right" size="mini" icon="el-icon-arrow-right"></el-button>
         </div>
       </div>
       <div class="list" ref="list">
         <card-special></card-special>
         <card-special></card-special>
-        <card-special></card-special>
-        <card-special></card-special>
-        <card-special></card-special>
-        <card-special></card-special>
-        <card-special></card-special>
-        <card-special></card-special>
-        <card-special></card-special>
+        <!--        <card-special></card-special>-->
+        <!--        <card-special></card-special>-->
+        <!--        <card-special></card-special>-->
+        <!--        <card-special></card-special>-->
+        <!--        <card-special></card-special>-->
+        <!--        <card-special></card-special>-->
+        <!--        <card-special></card-special>-->
+      </div>
+      <div class="bottomImg">
+        <img src="@/assets/bottomImg.webp" alt="">
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
 import HightLight from "@/components/common/hightLight/HightLight";
 import cardSpecial from "@/components/activityList/cardSpecial";
+
 export default {
   name: "activity",
-  components:{
+  components: {
     HightLight,
     cardSpecial
   },
-  methods:{
-    right(){
-      // console.log(this.$refs.  )
+  data() {
+    return {
+      leftLength: 0,
+      //rightLength:0
+    }
+  },
+  mounted() {
+    console.log(this.$refs)
+  },
+  methods: {
+    right() {
+      this.leftLength = this.leftLength - 235 - 14
+      this.$refs.list.style = `left:${this.leftLength}px`
+      if(this.leftLength<0){
+        this.$refs.leftBtn.$el.removeAttribute('disabled')
+        this.$refs.leftBtn.$el.classList.remove('is-disabled')
+      }
     },
-    left(){
-
+    left() {
+      this.leftLength = this.leftLength + 235 + 14
+      this.$refs.list.style = `left:${this.leftLength}px`
+      if (this.leftLength >= 0) {
+        this.$refs.leftBtn.$el.setAttribute('disabled', "disabled")
+        this.$refs.leftBtn.$el.classList.add('is-disabled')
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.activity{
+.activity {
   width: 1200px;
   margin: 0 auto;
-  .main{
+
+  .main {
+    position: relative;
+    z-index: 9990;
     background-color: #f5f5f5;
-    overflow-x: hidden;
-    .title{
+    padding: 0 20px;
+    overflow: hidden;
+
+    .title {
       display: flex;
       justify-content: space-between;
       height: 58px;
       line-height: 58px;
-      span{
+
+      span {
         font-size: 22px;
         color: #333333;
       }
-      .btn{
-        .el-button{
+
+      .btn {
+        .el-button {
           margin: 0;
         }
       }
     }
-    .list{
+
+    .list {
       position: relative;
       display: flex;
       margin-right: 14px;
+      transition: left 0.5s;
+      left: 0;
+      margin-bottom: 22px;
+    }
+
+    .bottomImg{
+      img{
+        width: 100%;
+      }
+
     }
   }
 }
